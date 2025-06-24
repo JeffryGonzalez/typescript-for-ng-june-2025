5edf957958f79d4de375584eea892d499bf4ce06
Jeff Gonzalez <jeff@hypertheory.com>
2025-06-24 14:09:56 -0400

    generics

--- src/syntax/advanced/generics.spec.ts
+++ src/syntax/advanced/generics.spec.ts
@@ -1,5 +1,20 @@
 import { describe, it, expect } from 'vitest';
 
 describe('Generics', () => {
-  it.todo('Write This');
+  it('intro', () => {
+    const numbers = [12, 18, 32, 64];
+
+    function head<TheKindOfArray>(nums: TheKindOfArray[]): TheKindOfArray {
+      return nums[0];
+    }
+
+    const first = head(numbers);
+
+    expect(first).toBe(12);
+
+    const names = ['bruce', 'carl', 'jill'];
+
+    const firstName = head(names);
+    expect(firstName).toBe('bruce');
+  });
 });
